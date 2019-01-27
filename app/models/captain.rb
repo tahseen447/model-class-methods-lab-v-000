111dd0750617133701245e441c4fc28dccbb4707
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-27 22:56:29 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,10 +9,6 @@ class Captain < ActiveRecord::Base
     includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
-  def self.motorboat_operators
-     includes(boats: :classifications).where(classifications: {name: "Motorboat"})
-   end
-
    def self.talented_seafarers
      where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
    end
